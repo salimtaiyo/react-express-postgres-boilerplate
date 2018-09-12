4b1b2373904b8dcbc070e48855f349091f101293
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-09-11 23:57:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req,res) => res.send('Hello world'));
+app.listen(port, () => console.log('The Express App is running in ' + port));

f10e4e769ed63d9e1ca4d719ff21237d9f28406c
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-09-13 23:56:45 -0400

    basic react express connection complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    yo:[]
+  }
+  componentDidMount() {
+    fetch('/hello')
+      .then(res => res.json())
+      .then(yo => this.setState({yo}))
+  }
   render() {
+    const yo =this.state.yo;
+    console.log(yo);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {yo.map( i => <li> {i.name}</li>)}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req,res) => res.send('Hello world'));
+app.get('/hello', (req,res) => {
+    const yo = [{ id:1, name: 'Shalom'}];
+    res.json(yo)
+});
 app.listen(port, () => console.log('The Express App is running in ' + port));
